ef0ad5b7047fe3fc896a31973ee7d6d3537b4c0c
Junghoon Park <eastnix@gmail.com>
2026-02-15 14:30:57 +0900

    update
    - update scene01,02
    - setting tile map
    - import tile&bg images

---
+++ Assets/02_scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void gotoBattleMapScene()
+    {
+        Debug.Log("btn clicked");
+        SceneManager.LoadScene("sc_01_battleMap");
+    }
+
+    public void gotoLobbyScene()
+    {
+        Debug.Log("btn clicked");
+        SceneManager.LoadScene("sc_00_lobby");
+    }
+}
