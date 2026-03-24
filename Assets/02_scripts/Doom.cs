7ce511dec9086ba4f9607a75643604a5fc6892be
Junghoon Park <eastnix@gmail.com>
2026-03-24 23:35:15 +0900

    update campaign
    add dwarf camp resources

--- Assets/02_scripts/Doom.cs
+++ Assets/02_scripts/Doom.cs
@@ -15,11 +15,14 @@ public enum eDoomLevel
 
 public class Doom : MonoBehaviour
 {
+    public eDoomType m_doomType;
     public string m_doomName;
     public int m_doomValue;
     public Image m_doomLevelBar;
     public eDoomLevel m_doomLevel;
     public TMP_Text text_doomLevel;
+
+    [SerializeField] private DoomEffect[] m_doomEffectArray = new DoomEffect[4];
 
     void Awake()
     {
@@ -29,7 +32,31 @@ public class Doom : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AssignDoomEffects(m_doomType);
+
+    }
+
+    public void AssignDoomEffects(eDoomType doomType)
+    {
+        for(int i=0; i <4; i++)
+        {
+            m_doomEffectArray[i] = new DoomEffect();
+        }
+
+        if(doomType == eDoomType.PlagueOfContamination)
+        {
+            m_doomEffectArray[0].isActive = false;
+            m_doomEffectArray[0].m_doomEffectName = "purifyingBreath";
+            m_doomEffectArray[1].isActive = false;
+            m_doomEffectArray[1].m_doomEffectName = "plagueTile";
+            m_doomEffectArray[2].isActive = false;
+            m_doomEffectArray[2].m_doomEffectName = "ringOfCorruption";
+            m_doomEffectArray[3].isActive = false;
+            m_doomEffectArray[3].m_doomEffectName = "plagueSpread";
+
+        }
+
+
     }
     
     public void UpdateDoom()
@@ -84,16 +111,36 @@ public class Doom : MonoBehaviour
         if(m_doomLevel == eDoomLevel.Stable)
         {
             //add purifyingBreath
+            m_doomEffectArray[0].isActive = true;
+            m_doomEffectArray[1].isActive = false;
+            m_doomEffectArray[2].isActive = false;
+            m_doomEffectArray[3].isActive = false;
         }
         else if(m_doomLevel == eDoomLevel.Threat)
         {
             //add plagueTile
+            m_doomEffectArray[0].isActive = false;
+            m_doomEffectArray[1].isActive = true;
+            m_doomEffectArray[2].isActive = false;
+            m_doomEffectArray[3].isActive = false;
+
         }
         else if(m_doomLevel == eDoomLevel.Critical)
         {
             //add ringOfCorruption, plagueSpread
+            m_doomEffectArray[0].isActive = false;
+            m_doomEffectArray[1].isActive = true;
+            m_doomEffectArray[2].isActive = true;
+            m_doomEffectArray[3].isActive = true;
         }
     }
 
+    public void InitDoomEffect()
+    {
+        m_doomEffectArray[0].isActive = false;
+        m_doomEffectArray[1].isActive = false;
+        m_doomEffectArray[2].isActive = false;
+        m_doomEffectArray[3].isActive = false;
+    }
     
 }
---
+++ Assets/02_scripts/DoomEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoomEffect
+{
+    public bool isActive = false;
+    public string m_doomEffectName;
+
+    public void PrintDoomEffectName()
+    {
+        Debug.Log(m_doomEffectName);
+    }
+}
